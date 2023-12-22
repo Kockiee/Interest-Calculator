92462e6972d47e76d569ec004449ec67be4df100
Miguel Eduardo <mgueli1208@gmail.com>
2023-12-22 15:01:16 -0300

    Explicação do que são juros compostos incluída

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { DefaultSeo } from 'next-seo';
 import './global.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -8,19 +7,6 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Calculadora de Juros Compostos Online | Finance Calc',
   description: 'Use nossa ferramenta online gratuita para calcular juros compostos de maneira rápida e eficiente.',
-  openGraph: {
-    title: 'Calculadora de Juros Compostos Online',
-    description: 'Use nossa ferramenta online gratuita para calcular juros compostos de maneira rápida e eficiente.',
-    images: [
-      {
-        url: '../../public/calc.jpg',
-        width: 824,
-        height: 482,
-        alt: 'Imagem da nossa calculadora de juros compostos online',
-      },
-    ],
-    siteName: 'Calculadora de Juros Compostos Online | Finance Calc',
-  }
 }
 
 
@@ -30,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body>
         <div className={`bg-black min-h-screen w-full ${inter.className} flex flex-row w-full text-gray-900 dark:text-gray-100 font-medium`}>
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,23 @@ export default function Home() {
         <div className='w-full min-h-screen flex flex-col items-center justify-center p-2'>
             <h1 className='text-3xl font-bold mb-4 text-center'>Calculadora de <span className='text-blue-600'>Juros Compostos</span></h1>
             <Calculator/>
+            <div className="max-w-2xl mx-auto border border-1 p-6 rounded shadow mt-4">
+                <h1 className="text-2xl font-bold mb-4">Juros Compostos</h1>
+
+                <p className="text-gray-500 mb-4">Juros compostos são uma maneira sofisticada de calcular o acréscimo de juros ao longo do tempo sobre uma quantia inicial. A peculiaridade desse método está na adição dos juros acumulados ao principal, resultando em um novo montante sobre o qual os juros subsequentes são calculados.</p>
+
+                <p className="text-gray-500 mb-4">A fórmula que governa esse processo é:</p>
+
+                <p className="text-gray-500 mb-4">
+                  A = P( 1 + r )^t
+                </p>
+
+                <p className="text-gray-500 mb-4">Aqui, A representa o montante total após o período t, P é o principal (o valor inicial), r é a taxa de juros por período, e t é o tempo em unidades de períodos.</p>
+
+                <p className="text-gray-500 mb-4">Diferentemente dos juros simples, onde os juros são calculados apenas sobre o principal original, os juros compostos incorporam os juros acumulados ao longo do tempo. Esse mecanismo resulta em um aumento exponencial do montante total à medida que o tempo avança.</p>
+
+                <p className="text-gray-500 mb-4">A aplicação dos juros compostos é prevalente em contextos financeiros, como em contas de poupança, investimentos e empréstimos. Seu impacto expressivo na expansão dos investimentos ao longo do tempo permite que os ganhos de juros se acumulem sobre os juros previamente conquistados.</p>
+            </div>
         </div>
     )
 }
